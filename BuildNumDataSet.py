c62b81bf60455244fe34ae94826a0e914e7b0533
zbf <hgkdzbf6@qq.com>
2018-04-26 21:02:11 +0800

    hello world

--- BuildNumDataSet.py
+++ BuildNumDataSet.py
@@ -1,29 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import os
+import os.path as ops
+import argparse
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 需要先在Output_Path下建好0-9 a-z的文件夹
+# 需要先在output_dir 下建好0-9 a-z的文件夹
 # 运行后按空格
 # 鼠标框选区域后按下其对应数字或字母按键
 # ESC:退出 c:清除图上的框 空格回车:下一张图
 # z:撤销 需要输入过数字字母后再按
-# 圈出的图片会保存到Output_Path下的文件夹内
+# 圈出的图片会保存到output_dir 下的文件夹内
 # 格式为xx_i.jpg xx为来源图片 i是该图中第i个圈出的数字或字母
 # 包含图片是啥和在原图中位置信息的 test.txt
 
-drag = False # 鼠标左键是不是按下
-inputComplete = True
-ix,iy = -1,-1
-px,py = -1,-1
-nums = []
 
-imagedir = "../images/ag/ag"
-suffix = ".jpg"
-Output_Path = "../images/ag/ag"
-
-class number(object):
+class Number(object):
     """store num pic and position value"""
     # pic: picture
     # position: (xmin,ymin,xmax,ymax)
@@ -35,93 +29,144 @@ class number(object):
     def setNumValue(self,val):
         self.value = val
 
-# 鼠标回调函数
-def MouseCallBack(event,x,y,flags,param):
-    global ix,iy,drag,px,py,img,inputComplete
-    if event == cv2.EVENT_LBUTTONDOWN:
-        if inputComplete:
-            # print("down",x,y)
-            drag = True
-            ix,iy = x,y
-            inputComplete = False
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if drag == True:
-            temp = img.copy()
-            cv2.rectangle(temp,(ix,iy),(x,y),(255,255,255),2)
-            cv2.imshow("image",temp)
-    elif event == cv2.EVENT_LBUTTONUP:
-        if drag:
-            # print("up",x,y)
-            drag = False
-            cv2.rectangle(img,(ix,iy),(x,y),(255,255,255),2)
-            undoimgs.append(img.copy())
-            cv2.imshow("image",img)
-            xmin,ymin,xmax,ymax = min(ix,x),min(iy,y),max(ix,x),max(iy,y)
-            nums.append(number(origin[ymin:ymax,xmin:xmax],(xmin,ymin,xmax,ymax)))
-            cv2.imshow("num",nums[-1].pic)
-        else:
-            print("please input number or character")
+class Cut(object):
+    def __init__(self, image_dir, output_dir):
+        self.ix=-1
+        self.iy=-1
+        self.drag=False
+        self.px=-1
+        self.py=-1
+        self.undoimgs=[]
+        self.nums=[]
+        self.inputComplete=True
+        self.suffix='.jpg'
+        self.image_dir=image_dir
+        self.output_dir=output_dir
+        self.parse_image(image_dir,output_dir)
 
-def save(num,Outdir,imagedir,imagei):
-    saveFile = open(imagedir + str(imagei) + '.txt', 'w')
-    for n in range(len(num)):
-        outname = Outdir + num[n].value + "/" + str(imagei) + "_" + str(n) + ".jpg"
-        cv2.imwrite(outname,num[n].pic)
-        saveFile.write(num[n].value + " "
-                + str(num[n].position[0]) + " " + str(num[n].position[1]) + " "
-                + str(num[n].position[2]) + " " + str(num[n].position[3]) + "\n"
-        )
+    # 鼠标回调函数
+    def MouseCallBack(self,event,x,y,flags,param):
+        # global ix,iy,drag,px,py,img,inputComplete
+        if event == cv2.EVENT_LBUTTONDOWN:
+            if self.inputComplete:
+                # print("down",x,y)
+                self.drag = True
+                self.ix,self.iy = x,y
+                self.inputComplete = False
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if self.drag == True:
+                temp = self.img.copy()
+                cv2.rectangle(temp,(self.ix,self.iy),(x,y),(255,255,255),2)
+                cv2.imshow("image",temp)
+        elif event == cv2.EVENT_LBUTTONUP:
+            if self.drag:
+                # print("up",x,y)
+                self.drag = False
+                cv2.rectangle(self.img,(self.ix,self.iy),(x,y),(255,255,255),2)
+                self.undoimgs.append(self.img.copy())
+                cv2.imshow("image",self.img)
+                xmin,ymin,xmax,ymax = min(self.ix,x),min(self.iy,y),max(self.ix,x),max(self.iy,y)
+                self.nums.append(Number(self.origin[ymin:ymax,xmin:xmax],(xmin,ymin,xmax,ymax)))
+                cv2.imshow("num",self.nums[-1].pic)
+            else:
+                print("please input Number or character")
 
+    def save(self):
+        saveFile = open(ops.join(self.image_dir , str(self.index) + '.txt'), 'w')
+        for n in range(len(self.nums)):
+            outname = ops.join(self.output_dir , self.nums[n].value + "/" + str(self.index) + "_" + str(n) + ".jpg")
+            cv2.imwrite(outname,self.nums[n].pic)
+            saveFile.write(self.nums[n].value + " "
+                    + str(self.nums[n].position[0]) + " " + str(self.nums[n].position[1]) + " "
+                    + str(self.nums[n].position[2]) + " " + str(self.nums[n].position[3]) + "\n"
+            )
 
+    def test_dir(self,path):
+        for i in range(0,10):
+            new_path = ops.join(path,str(i))
+            if not ops.exists(new_path):
+                os.makedirs(new_path)
+        
+        for i in range(0,26):
+            new_path=ops.join(path,chr(ord('a')+i))
+            if not ops.exists(new_path):
+                os.makedirs(new_path)
 
-imagei = 0
-cv2.namedWindow("image",0)
-cv2.setMouseCallback('image',MouseCallBack)
-f = open(Output_Path+'test.txt', 'a')
-while imagei < 124:
-    k = cv2.waitKey() & 0xFF
-    if k == 27:
-        #ESC
+    def parse_image(self,image_dir, output_dir):
+        self.test_dir(output_dir)
+        self.index = 0
+        cv2.namedWindow("image",0)
+        cv2.setMouseCallback('image',self.MouseCallBack)
+        f = open(ops.join(output_dir ,'test.txt'), 'a')
+        while self.index < 124:
+            k = cv2.waitKey() & 0xFF
+            if k == 27:
+                #ESC
+                f.close()
+                break
+            if not self.inputComplete:
+                if 48 <= k <= 57 or 65 <= k <= 90 or 97 <= k <= 122:
+                    # 数字字母
+                    self.nums[-1].setNumValue(chr(k))
+                    print(self.index,"set Number picture as",self.nums[-1].value)
+                    self.inputComplete = True
+            elif k == 13:
+                # Enter
+                # 标记下一张图片
+                if self.nums:
+                    self.save()
+                self.index += 1
+                image_name = ops.join(image_dir , str(self.index) + self.suffix)
+                print('try to open {:s}'.format(image_name))
+                self.origin = cv2.imread(image_name,0)
+                self.img = self.origin.copy()
+                self.nums, self.undoimgs = [], []
+                self.undoimgs.append(self.origin)
+                cv2.imshow("image",self.origin)
+            elif k == 32:
+                # Space
+                # 标记下一个数字
+                if self.nums:
+                    self.save()
+                # self.index += 1
+                image_name = ops.join(image_dir , str(self.index) + self.suffix)
+                print('try to open {:s}'.format(image_name))
+                self.origin = cv2.imread(image_name,0)
+                self.img = self.origin.copy()
+                # self.nums, self.undoimgs = [], []
+                self.undoimgs.append(self.origin)
+                cv2.imshow("image",self.origin)
+            elif k == ord('c'):
+                # clear
+                self.img = self.origin.copy()
+                cv2.imshow("image",self.img)
+            elif k == ord("z"):
+                # undo
+                if len(self.undoimgs) > 1:
+                    self.undoimgs.pop()
+                    self.nums.pop()
+                    self.img = self.undoimgs[-1].copy()
+                    cv2.imshow("image",self.img)
+                    if self.nums != []:
+                        cv2.imshow("num",self.nums[-1].pic)
         f.close()
-        break
-    if not inputComplete:
-        if 48 <= k <= 57 or 65 <= k <= 90 or 97 <= k <= 122:
-            # 数字字母
-            nums[-1].setNumValue(chr(k))
-            print(imagei,"set number picture as",nums[-1].value)
-            inputComplete = True
-    elif k == 13:
-        # Enter
-        if nums:save(nums,Output_Path,imagedir,imagei)
-        imagei += 1
-        image_name = imagedir + str(imagei) + suffix
-        origin = cv2.imread(image_name,0)
-        img = origin.copy()
-        nums, undoimgs = [], []
-        undoimgs.append(origin)
-        cv2.imshow("image",origin)
-    elif k == 32:
-        # Space
-        if nums:save(nums,Output_Path,imagedir,imagei)
-        imagei += 1
-        image_name = imagedir + str(imagei) + suffix
-        origin = cv2.imread(image_name,0)
-        img = origin.copy()
-        nums, undoimgs = [], []
-        undoimgs.append(origin)
-        cv2.imshow("image",origin)
-    elif k == ord('c'):
-        # clear
-        img = origin.copy()
-        cv2.imshow("image",img)
-    elif k == ord("z"):
-        # undo
-        if len(undoimgs) > 1:
-            undoimgs.pop()
-            nums.pop()
-            img = undoimgs[-1].copy()
-            cv2.imshow("image",img)
-            if nums != []:
-                cv2.imshow("num",nums[-1].pic)
 
-f.close()
+def init_args():
+    '''
+    :return:
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_dir', type=str, help='where you store the image')
+    parser.add_argument('--output_dir', type=str, help='where you save the output')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    # init args
+    args = init_args()
+    if not ops.exists(args.image_dir):
+        raise ValueError("image {:s} doesn't exist".format(args.image_dir))
+    
+    cut= Cut(image_dir=args.image_dir, output_dir=args.output_dir)
+    # if not ops.exists(args.output_dir):
+    #     raise ValueError("output directory {:s} doesn't exist".format(args.output_dir))
